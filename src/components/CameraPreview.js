5dbe04bd066b5f4f2022ab9e1f6cbff6b165a373
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-19 15:52:59 +0700

    add: camera preview component

---
+++ src/components/CameraPreview.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native'
+
+const CameraPreview = ({photo, retakePicture, savePhoto}) => {
+  return (
+    <View
+      style={styles.container}>
+      <ImageBackground
+        source={{ uri: photo.uri }}
+        style={styles.imageContainer}>
+        <View
+          style={styles.optionsContainer}>
+          <View
+            style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={retakePicture}
+              style={styles.button}
+            >
+              <Text
+                style={styles.buttonText}
+              >
+                Re-take
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={savePhoto}
+              style={styles.button}
+            >
+              <Text
+                style={styles.buttonText}
+              >
+                Analyze Photo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default CameraPreview
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'transparent',
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  imageContainer: {
+    flex: 1
+  },
+  optionsContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 15,
+    justifyContent: 'flex-end'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    width: 130,
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 4
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  }
+})
