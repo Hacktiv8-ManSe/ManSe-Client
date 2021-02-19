7cc4df3daa728036b605882e45381d0b3ae79b94
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-19 10:11:10 +0700

    add: initial screen

---
+++ src/screens/BMRScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BMRScreen = () => {
+  return (
+    <View>
+      <Text>BMRScreen</Text>
+    </View>
+  )
+}
+
+export default BMRScreen
+
+const styles = StyleSheet.create({})
