34c2bb5efdabc9059e05116aed322f4bb1c3bb6f
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-20 11:26:30 +0700

    add: redux store

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import BottomNavigator from './src/routes/BottomNavigator'
+import store from './src/store'
+import { Provider } from 'react-redux'
 
 export default function App() {
   return (
-    <BottomNavigator />
+    <Provider store={store}>
+      <BottomNavigator />
+    </Provider>
   );
 }
