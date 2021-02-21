eb4fd5fcd350b955a1fbd415b88199357beae411
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-21 12:18:21 +0700

    add: loginscreen stack

--- src/routes/BottomNavigator.js
+++ src/routes/BottomNavigator.js
@@ -2,29 +2,26 @@ import React from "react"
 import { Ionicons } from "@expo/vector-icons"
 import {
   HomeScreen,
-  ExploreScreen,
   ScanScreen,
-  BMRScreen,
-  ProfileScreen
+  ProfileScreen,
+  LoginScreen
 } from '../screens'
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from "@react-navigation/stack"
 import { colors } from '../constants/colors'
 
-
 const Tab = createBottomTabNavigator()
 const MainStack = createStackNavigator()
 
-const BottomNavigator = () => (
+export const BottomNavigator = () => (
   <Tab.Navigator
     initialRouteName="Home"
     tabBarOptions={{
       activeTintColor: colors.activeTintColor,
       style: {
         backgroundColor: colors.primaryColor,
-        borderTopColor: "transparent",
-        // marginBottom: 25,
+        borderTopColor: "transparent"
       },
     }}
   >
@@ -39,32 +36,12 @@ const BottomNavigator = () => (
       }}
     />
     <Tab.Screen
-      name="Explore"
-      component={ExploreScreen}
-      options={{
-        tabBarLabel: "Explore",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="ios-search" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
       name="Scan"
       component={ScanScreen}
       options={{
         tabBarLabel: "Scan",
         tabBarIcon: ({ color, size }) => (
           <Ionicons name="ios-camera" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="BMR"
-      component={BMRScreen}
-      options={{
-        tabBarLabel: "BMR",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="ios-calculator" color={color} size={size} />
         ),
       }}
     />
@@ -85,6 +62,7 @@ export default () => (
   <NavigationContainer>
     <MainStack.Navigator headerMode="none">
       <MainStack.Screen name="App" component={BottomNavigator} />
+      <MainStack.Screen name="LoginScreen" component={LoginScreen} />
     </MainStack.Navigator>
   </NavigationContainer>
 )
